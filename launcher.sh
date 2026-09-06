74826867f91927f6a12e714efa0e136e52be2ff2
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:17:55 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ARCH LINUX NATIVE AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/ultimate_suite/zero-raft-arch-linux
 git pull origin main --quiet
-python3 zero_raft_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_raft_gui.py
